2ac672fb940238dddbd8a6fea82bf601224d4b61
leejunho0909 <leejunho12316@gmail.com>
2026-05-04 23:06:53 +0900

    README

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Video_Generator_Agent import run
 
 # ppt 경로, prompt 설정
-PPTX_PATH = "./ppt_examples/sample1.pptx"  # 실행할 PPT 파일 경로
+PPTX_PATH = "./ppt_examples/sample2.pptx"  # 실행할 PPT 파일 경로
 prompt={
     "voice": "alloy",
     "tone": "명확하고 연설하는 듯한 강렬한 톤",
